766ebb42ec4165eaf1769d35949f110cc4d93650
Adrian Ćwiąkała <adrian.cwiakala@febrit.co>
2022-11-02 16:38:06 +0100

    feat: table od points, wip: end round

--- libs/web/ui-game/src/lib/phases/presentation/Presentation.tsx
+++ libs/web/ui-game/src/lib/phases/presentation/Presentation.tsx
@@ -4,6 +4,7 @@ import {
   Affix,
   Box,
   BoxProps,
+  Button,
   Center,
   Grid,
   Group,
@@ -19,7 +20,8 @@ import { Socket } from 'socket.io-client';
 
 import { motion, useAnimationControls } from 'framer-motion';
 import { Card } from '@hypnos/web/ui-game-controls';
-import { ActionType } from 'libs/web/network/src/lib/types';
+import { ActionType, GameEntity } from 'libs/web/network/src/lib/types';
+import { ShowPoints } from './ShowPoints';
 
 export const Presentation = () => {
   const context = useContext(GameContext);
@@ -42,16 +44,40 @@ export const Presentation = () => {
 
     if (!state.me.player.isMaster) return;
 
-    // if (cardIndex === context?.[0].players.length) {
-    //   // dispatch({ type: ActionType.initRound, payload: null });
+    if (cardIndex === context?.[0].players.length + 1) {
+      // dispatch({ type: ActionType.initRound, payload: null });
+      console.log('End');
 
-    //   return;
-    // }
+      dispatch({
+        type: ActionType.setGame,
+        payload: {
+          ...state,
+          players: state.players.map((p) => {
+            const pdata = data.find((d) => d.ownerSID === p.socketId);
+            const sum = pdata
+              ? pdata?.forgeryPoints +
+                pdata?.guessPoints +
+                pdata?.narrationPoints
+              : 0;
+
+            return {
+              ...p,
+              points: (p.points ?? 0) + sum,
+            };
+          }),
+        } as GameEntity,
+      });
+
+      dispatch({ type: ActionType.initRound, payload: null });
+
+      return;
+    }
 
     (state.me.socket as Socket).emit(
       PresentationPhaseEvents.setScene,
       context?.[0].roomCode,
-      cardIndex === null ? 0 : (cardIndex + 1) % (state.players.length + 1)
+      cardIndex === null ? 0 : cardIndex + 1
+      //  % (state.players.length + 1)
     );
   };
 
@@ -80,93 +106,69 @@ export const Presentation = () => {
   return (
     <Center sx={{ height: 'calc(100vh - 50px)' }}>
       <Stack justify="center" align="center">
-        {cardIndex === null && context?.[0].me.player.isMaster && (
-          <Center
-            sx={{ minHeight: '100px', cursor: 'pointer' }}
-            onClick={notifyNextScene}
-          >
-            <motion.div initial={{ y: '-501px' }} animate={{ y: '0px' }}>
-              <Text sx={{ fontStyle: 'italic' }} color={'dimmed'} size="xl">
-                Click to start Presentations
-              </Text>
-            </motion.div>
-          </Center>
-        )}
-
-        {cardIndex === null && !context?.[0].me.player.isMaster && (
-          <Text>Waiting for master to start presentation of votes</Text>
-        )}
-
         {cardIndex !== null && cardIndex < (context?.[0].players.length ?? 0) && (
-          <>
-            <Group>
-              <Stack>
-                <motion.div
-                  initial={{ y: '-1000px' }}
-                  animate={nicknameControls}
-                >
-                  <Stack spacing="xs" align="center">
-                    {isFinal ? (
-                      <Text size={18}>The actual card:</Text>
-                    ) : (
-                      <Text>Card from:</Text>
-                    )}
-                    <Text size={30} variant="gradient">
-                      {
-                        context?.[0].players.find(
-                          (p) => p.socketId === currentRecord?.ownerSID
-                        )?.name
-                      }
-                    </Text>
-                  </Stack>
-                </motion.div>
-                <motion.div initial={{ y: '-1000px' }} animate={cardControls}>
-                  <Card
-                    src={currentRecord?.card ?? ''}
-                    // onClick={notifyNextScene}
-                  />
-                </motion.div>
-              </Stack>
-
-              <Stack>
-                <motion.div initial={{ y: '-1000px' }} animate={votesControls}>
-                  <Text>Votes on this card:</Text>
-                  {currentRecord && currentRecord?.votes.length > 0 ? (
-                    currentRecord?.votes.map((vote, i) => {
-                      return (
-                        <motion.div
-                          key={i}
-                          initial={{ scale: 0 }}
-                          animate={{ scale: 1 }}
-                          transition={{ delay: i * 0.2 }}
-                        >
-                          <Group>
-                            <Text>
-                              {
-                                context?.[0].players.find(
-                                  (p) => p.socketId === vote.playerSID
-                                )?.name
-                              }
-                            </Text>
-                            {/* <Text>{isFinal ? '' : '+3'}</Text> */}
-                          </Group>
-                        </motion.div>
-                      );
-                    })
+          <Group>
+            <Stack>
+              <motion.div initial={{ y: '-1000px' }} animate={nicknameControls}>
+                <Stack spacing="xs" align="center">
+                  {isFinal ? (
+                    <Text size={18}>The actual card:</Text>
                   ) : (
-                    <Text>No votes :/</Text>
+                    <Text>Card from:</Text>
                   )}
-                </motion.div>
-              </Stack>
-            </Group>
-          </>
+                  <Text size={30} variant="gradient">
+                    {
+                      context?.[0].players.find(
+                        (p) => p.socketId === currentRecord?.ownerSID
+                      )?.name
+                    }
+                  </Text>
+                </Stack>
+              </motion.div>
+              <motion.div initial={{ y: '-1000px' }} animate={cardControls}>
+                <Card
+                  src={currentRecord?.card ?? ''}
+                  // onClick={notifyNextScene}
+                />
+              </motion.div>
+            </Stack>
+
+            <Stack>
+              <motion.div initial={{ y: '-1000px' }} animate={votesControls}>
+                <Text>Votes on this card:</Text>
+                {currentRecord && currentRecord?.votes.length > 0 ? (
+                  currentRecord?.votes.map((vote, i) => {
+                    return (
+                      <motion.div
+                        key={i}
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        transition={{ delay: i * 0.2 }}
+                      >
+                        <Group>
+                          <Text>
+                            {
+                              context?.[0].players.find(
+                                (p) => p.socketId === vote.playerSID
+                              )?.name
+                            }
+                          </Text>
+                          {/* <Text>{isFinal ? '' : '+3'}</Text> */}
+                        </Group>
+                      </motion.div>
+                    );
+                  })
+                ) : (
+                  <Text>No votes :/</Text>
+                )}
+              </motion.div>
+            </Stack>
+          </Group>
         )}
 
         {cardIndex !== null &&
           cardIndex === (context?.[0].players.length ?? 0) && (
-            <>
-              <Text onClick={notifyNextScene}>Show points</Text>
-            </>
+            <ShowPoints data={data} />
           )}
 
         {context?.[0].me.player.isMaster && (
@@ -174,9 +176,9 @@ export const Presentation = () => {
             sx={{ minHeight: '100px', cursor: 'pointer' }}
             onClick={notifyNextScene}
           >
-            <motion.div initial={{ y: '-501px' }} animate={continueControls}>
+            <motion.div initial={{ opacity: 0 }} animate={continueControls}>
               <Text sx={{ fontStyle: 'italic' }} color={'dimmed'} size="xl">
-                Click to continue
+                {cardIndex === null ? 'Tap to start' : 'Tap to continue'}
               </Text>
             </motion.div>
           </Center>
@@ -198,11 +200,12 @@ export const Presentation = () => {
   );
 };
 
-interface PresentationRecord {
+export interface PresentationRecord {
   card: string;
   ownerSID: string;
   forgeryPoints: number;
   narrationPoints: number;
+  guessPoints: number;
   votes: {
     playerSID: string;
     points: number;
@@ -235,10 +238,21 @@ const usePresentationData = () => {
         const isNarrator =
           player.playerSID === context[0].round?.currentPlayerSID;
 
+        const actualCard = playerData.find(
+          (pd) => pd.playerSID === context[0].round?.currentPlayerSID
+        )?.ownedCardUrl;
+
         return {
           card: player.ownedCardUrl,
           ownerSID: player.playerSID,
           forgeryPoints: !isNarrator ? votes.length : 0,
+
+          guessPoints:
+            playerData.find((pd) => pd.playerSID === player.playerSID)
+              ?.votedCardUrl === actualCard
+              ? 2
+              : 0,
+
           narrationPoints:
             isNarrator &&
             votes.length > 0 &&
---
+++ libs/web/ui-game/src/lib/phases/presentation/ShowPoints.tsx
@@ -0,0 +1,109 @@
+import { GameContext } from '@hypnos/web/network';
+import { Group, Stack, Text, Table, TextProps, Tooltip } from '@mantine/core';
+import { useContext, useMemo } from 'react';
+import { PresentationRecord } from './Presentation';
+
+export interface ShowPointsProps {
+  data: PresentationRecord[];
+}
+
+export const ShowPoints = (props: ShowPointsProps) => {
+  const context = useContext(GameContext);
+
+  return (
+    <>
+      <Text variant="gradient" size="lg">
+        Round Summary
+      </Text>
+      <Table
+        title="Round summary"
+        horizontalSpacing="xl"
+        sx={{ textAlign: 'center' }}
+      >
+        <thead>
+          <tr>
+            <td></td> <td></td>
+            <td>
+              <Text>Points</Text>
+            </td>
+            <td>
+              <ToolTipped text="F" label="Forgery points" />
+            </td>
+            <td>
+              <ToolTipped text="N" label="Narration points" />
+            </td>
+            <td>
+              <ToolTipped text="G" label="Guess points" />
+            </td>
+            <td>
+              <Text>Sum</Text>
+            </td>
+            <td>
+              <Text>Total</Text>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {context?.[0].round?.playerData
+            .sort((a, b) => (a.points ?? 0) - (b.points ?? 0))
+            .map((pd, i) => {
+              const pdata = context[0].players.find(
+                (p) => p.socketId === pd.playerSID
+              );
+
+              const precord = props.data.find(
+                (e) => e.ownerSID === pd.playerSID
+              );
+              const sum = precord ? calcPoints(precord) : 0;
+
+              return (
+                <tr key={i}>
+                  <td>{`${i + 1}. `}</td>
+                  <td> {pdata?.name}</td>
+                  <td> {pdata?.points ?? 0}</td>
+                  <td>{zerofy(precord?.forgeryPoints)}</td>
+                  <td>{zerofy(precord?.narrationPoints)}</td>
+                  <td>{zerofy(precord?.guessPoints)}</td>
+                  <td>{precord && printSum(sum)}</td>
+                  <td>
+                    <Text color="cyan">{(pdata?.points ?? 0) + sum}</Text>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+interface ToolTippedProps {
+  text: string;
+  label: string;
+}
+
+const ToolTipped = (props: ToolTippedProps) => {
+  return (
+    <Tooltip
+      withArrow
+      label={props.label}
+      events={{ hover: true, focus: true, touch: true }}
+    >
+      <Text>{props.text}</Text>
+    </Tooltip>
+  );
+};
+
+const printSum = (value: number) => {
+  return (
+    <Text align="right" color={value ? 'green' : 'red'}>
+      {value ? `+${value}` : '0'}
+    </Text>
+  );
+};
+
+const zerofy = (value: number | undefined) => (value === 0 ? '' : value);
+
+const calcPoints = (rec: PresentationRecord) => {
+  return rec.forgeryPoints + rec.forgeryPoints + rec.narrationPoints;
+};
