77b83c2b45d5a57edae28ebcea9c59640fa4d0e3
Piotr Chuchla <piotr.chuchla@febrit.co>
2022-11-05 14:57:55 +0100

    feat: swagger api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,11 +5,22 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('Manage users')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 
