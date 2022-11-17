3d36cbbf9d1284474c846c9b8f1560fb7450c58f
Adrian Ćwiąkała <adrian.cwiakala@febrit.co>
2022-11-17 12:34:15 +0100

    wip: Game Over

--- libs/web/ui-game/src/lib/phases/GameOver.tsx
+++ libs/web/ui-game/src/lib/phases/GameOver.tsx
@@ -1,11 +1,34 @@
-import { Center, Grid, Stack, Title } from '@mantine/core';
+import { GameContext } from '@hypnos/web/network';
+import {
+  Center,
+  Grid,
+  Group,
+  Progress,
+  Stack,
+  Title,
+  Text,
+} from '@mantine/core';
+import { useContext, useEffect, useMemo } from 'react';
 
 export const GameOver = () => {
+  const context = useContext(GameContext);
+
+  const data = useMemo(() => {
+    if (!context) return;
+
+    return {
+      max: context?.[0].players.reduce((max, curr) => {
+        const points = curr.points ?? 0;
+        return points > max ? points ?? 0 : max;
+      }, 0),
+    };
+  }, [context]);
+
   return (
     <Grid m={0} p={0}>
       <Grid.Col m={0} p={0}>
         <Center sx={{ height: '100vh' }}>
-          <Stack>
+          <Stack align="center">
             <Title
               order={2}
               variant="gradient"
@@ -13,6 +36,20 @@ export const GameOver = () => {
             >
               Game Over!
             </Title>
+            {context &&
+              context[0].players.map((p, k) => {
+                return (
+                  <Group key={k}>
+                    <Progress
+                      radius="xs"
+                      size="lg"
+                      value={(p.points ?? 0) / (data?.max ?? 1)}
+                      sx={{ width: '300px' }}
+                    />
+                    <Text>{p.name}</Text>
+                  </Group>
+                );
+              })}
           </Stack>
         </Center>
       </Grid.Col>
