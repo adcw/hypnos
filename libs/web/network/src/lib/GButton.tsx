a37c0d973ee4416d24ae2dfb094d0f9ca5171572
Adrian Ćwiąkała <adrian.cwiakala@febrit.co>
2022-12-07 20:59:29 +0100

    style: button restyling

--- libs/web/network/src/lib/GButton.tsx
+++ libs/web/network/src/lib/GButton.tsx
@@ -1,48 +1,38 @@
-import { UnstyledButton, UnstyledButtonProps } from '@mantine/core';
+import { Sx, UnstyledButton, UnstyledButtonProps } from '@mantine/core';
 import { ReactNode } from 'react';
 
 export interface GButtonProps extends UnstyledButtonProps {
-    onClick(): void;
-    width?: number;
+  onClick(): void;
+  width?: number;
 }
 
 export function GButton(props: GButtonProps) {
+  const style: Sx = {
+    width: `${props.width}px`,
+    textAlign: 'center' as const,
+    color: '#FFEEB0',
+    fontSize: '18px',
+    background: '#101113',
+    padding: '10px 15px',
+    borderRadius: '15px',
+    transition: 'text-shadow 0.3s',
 
-    const style = {
-        width: `${props.width}px`,
-        textAlign: "center" as const,
-        color: "#FFEEB0",
-        fontSize: "18px",
-        background: "#241E1E",
-        padding: "10px 15px",
-        borderRadius: "15px",
+    '&:enabled': {},
+    '&:disabled': {
+      color: '#B7B88F',
+      background: '#101113',
+    },
+    '&:active': {
+      boxShadow: 'inset 0 0 10px #000000',
+      transform: 'scale(0.95)',
+    },
+    '&:hover': {
+      background: '#101113',
+      textShadow: '0 0 10px #FFEEB0',
+    },
+  };
 
-        '&:enabled': {
-            textShadow: "0 0 10px #FFEEB0",
-            transition: "text-shadow 0.3s",
-        },
-        '&:disabled': {
-            color: "#B7B88F",
-            background: "#101113"
-        },
-        '&:active': {
-            borderTop: "3px solid black"
-        },
-        '&:hover': {
-          background: "#101113"
-        }
-
-    }
-
-    return (
-        <UnstyledButton
-            {...props}
-            sx={style}
-        />
-    );
+  return <UnstyledButton {...props} sx={style} />;
 }
-
-
-
 
 export default GButton;
